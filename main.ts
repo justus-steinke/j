784c6246d1957c42d5c8472637e3de5ed2cb1f7d
justus-steinke <132341698+justus-steinke@users.noreply.github.com>
2023-05-02 10:45:36 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,15 @@
+let ZUFALL = 0
 basic.forever(function () {
-	
+    if (input.pinIsPressed(TouchPin.P0)) {
+        ZUFALL = randint(0, 1)
+    }
+    if (ZUFALL == 0) {
+        basic.showIcon(IconNames.Happy)
+    }
+    if (ZUFALL == 0) {
+        basic.showIcon(IconNames.Sad)
+    }
+    control.waitMicros(6000)
+    ZUFALL = 2
+    basic.clearScreen()
 })
